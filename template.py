1d70f17f1cde6735a25357bb5df975aa790ae101
einaeffchen <github@dummer.dev>
2023-12-09 12:35:45 +0900

    Add utils and template

---
+++ template.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from collections import Counter
+from utils import get_data
+
+TEST_DATA = [
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483",
+]
+
+
+def task01(input_data: list[str], test=True):
+    pass
+
+
+def task02(input_data: list[str], test=True):
+    pass
+
+
+task01(TEST_DATA)
+print(task01(get_data(7), test=False))
+task02(TEST_DATA)
+print(task02(get_data(7), test=False))
--- utils.py
+++ utils.py
@@ -1,17 +1,23 @@
 import requests
 from login import cookie
 from pathlib import Path
+from datetime import datetime
 
 
 def get_data(day: int):
     p = Path(__file__).parent / "input.txt"
-    if not p.is_file():
-        data = requests.get(
-            f"https://adventofcode.com/2023/day/{day}/input", cookies=cookie
-        ).text
-        with open("input.txt", "w") as f_out:
-            f_out.write(data)
-    else:
-        with open(p) as f_in:
-            data = f_in.read()
+    if p.is_file():
+        creation_date = datetime.fromtimestamp(p.stat().st_mtime)
+        if creation_date.day == day:
+            with open(p) as f_in:
+                data = f_in.read()
+            return data.split("\n")
+    data = requests.get(
+        f"https://adventofcode.com/2023/day/{day}/input", cookies=cookie
+    ).text
+    with open("input.txt", "w") as f_out:
+        f_out.write(data)
     return data.split("\n")
+
+
+get_data(8)
